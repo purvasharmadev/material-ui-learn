80024be9e6b5a2f006b7cb8875c8a90e97dcd8f0
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-07-08 10:09:21 +0530

    creating custom components 1. how to create styled components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import "./styles.css";
-import { Box, Stack, Button } from "@mui/material";
+import { Box, Stack, Button, styled } from "@mui/material";
 import Sidebar from "./Components/sidebar/sidebar";
 import Feed from "./Components/feed/feed";
 import Rightbar from "./Components/rightbar/rightbar";
 export default function App() {
+  //BOX: acts like a div with predefined margin and padding
+  // Stack: a flex container for 1d layout
+  // Button: variant: type of btn, sx={{used to give custom styles}}
+
+  //  How to use emotion-react or styled-components to create custom components
+  /*
+  1. import {styled} from "@mui/material"
+  2.define a const 
+  3. use styled("Any MUI component/any HTML element",{
+    //give your styles here
+  })
+  */
+
+  const MyBtn = styled(Button)({
+    backgroundColor: "black",
+    color: "white",
+    "&:hover": {
+      backgroundColor: "grey",
+      color: "black"
+    },
+    "&:disabled": {
+      backgroundColor: "blue"
+    }
+  });
+
   return (
     <>
       <Box>
         <h2>Navbar</h2>
-        <Button
+        <MyBtn disabled>click here</MyBtn>
+        {/* <Button
           variant="contained"
           sx={{
             color: "red",
@@ -19,7 +45,7 @@ export default function App() {
           }}
         >
           Click
-        </Button>
+        </Button> */}
 
         <Stack direction="row" spacing={2} justifyContent={"space-between"}>
           <Sidebar />
